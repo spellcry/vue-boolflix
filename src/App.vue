104eb997db95b8036b23715ecb71484c67b89200
spellcry <sangiothebest@hotmail.com>
2022-09-09 16:26:41 +0200

    refactor milestone 1

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+  import axios from 'axios'
+  import state from './store'
   import MainHeader from './components/HeaderComponent.vue'
   import MainContent from './components/MainContentComponent.vue'
 
@@ -15,6 +17,38 @@
     components: {
       MainHeader,
       MainContent,
+    },
+    data() {
+      return {
+        baseUri: 'https://api.themoviedb.org/3',
+        apiKey: '5702129c9507efdfcd526d3dbc3febce',
+        language: 'it-IT',
+      }
+    },
+    computed: {
+      query() {
+        return state.query;
+      }
+    },
+    watch: {
+      query() {
+        if ( this.query !== '' )
+          this.getFilms();
+      },
+    },
+    methods: {
+      getFilms() {
+        axios.get(`${this.baseUri}/search/movie`,{
+        params: {
+          api_key: this.apiKey,
+          query: this.query,
+          language: this.language,
+        }
+        })
+        .then((res) => {
+          state.films = res.data.results;
+        });
+      },
     },
   }
 </script>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,45 +3,26 @@
         <h1 class="main-header__title">BoolFlix</h1>
         <div class="search">
             <input type="text" class="search__input" v-model="query">
-            <button @click="getFilms" class="search__btn">Cerca</button>
+            <button @click="setQuery" class="search__btn">Cerca</button>
         </div>
     </header>
 </template>
 
 <script>
-    import axios from 'axios'
     import state from '../store'
 
     export default {
         data() {
             return {
-                baseUri: 'https://api.themoviedb.org/3',
-                apiKey: '5702129c9507efdfcd526d3dbc3febce',
                 query: '',
-                language: 'it-IT',
-            }
-        },
-        watch: {
-            query() {
-                state.query = this.query;
             }
         },
         methods: {
-            getFilms() {
-                axios.get(`${this.baseUri}/search/movie`,{
-                params: {
-                    api_key: this.apiKey,
-                    query: this.query,
-                    language: this.language,
-                }
-                })
-                .then((res) => {
-                    state.films = res.data.results;
-                });
-            },
-        },
+            setQuery() {
+                state.query = this.query;                
+            }
+        }
     }
-
 </script>
 
 <style lang="scss" scoped>
